516df990b8f602b4272e20be3f8cd47970b2ae4b
stedy <zstednic@fhcrc.org>
2016-08-16 08:48:35 -0700

    return results to file for D3 barchart

--- get_stats.py
+++ get_stats.py
@@ -1,20 +1,25 @@
+import argparse
 from splinter import Browser
 from time import sleep
 from datetime import date
 from bs4 import BeautifulSoup
-import os
 
+parser = argparse.ArgumentParser(description = """Summarize number of episodes
+        of This American Life marked as Heard by year""")
+parser.add_argument('user', help = "Username for http://wwww.thisamericanlife.org")
+parser.add_argument('passwd', help = "User password for http://wwww.thisamericanlife.org")
+args = parser.parse_args()
 
 with Browser() as browser:
     slug = "http://www.thisamericanlife.org"
     url = slug + "/user/login"
     browser.visit(url)
-    browser.fill('name', 'z11k')
-    browser.fill('pass', 'pwd_2012')
+    browser.fill('name', args.user)
+    browser.fill('pass', args.passwd)
     button = browser.find_by_xpath('/html/body/div[2]/div[2]/div[3]/div[1]/form/div/div/input[3]').click()
 
     yearly_pcts = []
-    years = range(2015, date.today().year + 1)
+    years = range(1995, date.today().year + 1)
     for year in years:
         episode_count, heard_count = 0, 0
 
@@ -29,11 +34,8 @@ with Browser() as browser:
         for te in total_episodes:
             episode_count += 1
         yearly_pcts.append((year, float(heard_count)/episode_count))
-    print yearly_pcts
 
-with open("results.txt", "wb") as outfile:
+with open("TAL.csv", "wb") as outfile:
+    outfile.write("year,percentage\n")
     for x in yearly_pcts:
         outfile.write("{},{}\n".format(x[0], x[1]))
-
-    #then view file
-    #browser.visit("file:///" + os.getcwd() + "/index.html")
